3ff5183744cefd33da49c6d85376291baf69b566
Stefan Åhlander <cs.ahlander@gmail.com>
2019-11-05 09:15:59 +0100

    Add .scss file and initiate work on gameController

--- js/script.js
+++ js/script.js
@@ -7,27 +7,59 @@ import winView from "./views/win.js";
 
 const gameBoard = document.querySelector("#app");
 
-console.log(wordArray[0]);
+let state = {
+  guesses: 0
+};
 
-const renderView = (view) => {
-    gameBoard.innerHTML = view;
+function renderView(view) {
+  gameBoard.innerHTML = view;
 }
 
-const init = () => {
-    let state = {
-        guesses: 0
-    };
-
-    renderView(initialView);
-    initialControler();
+function init() {
+  renderView(initialView);
+  initialController();
 }
 
-const initialControler = () => {
-    let startBtn = document.querySelector("#startBtn");
-    startBtn.addEventListener("click", () => {
-        renderView(gameView);
-        gameController();
-    });
+function initialController() {
+  let startBtn = document.querySelector("#startBtn");
+
+  function startGame() {
+    startBtn.removeEventListener("click", startGame);
+    renderView(gameView);
+    gameController();
+  };
+
+  startBtn.addEventListener("click", startGame);
+}
+
+function renderCurrentWord(word, currentWord) {
+  word.forEach((char, index) => {
+    let newChar = document.createElement("span");
+    newChar.innerText = "?";
+    currentWord.appendChild(newChar);
+  });
+}
+
+function renderGameBoard(gameBoard) {
+
+}
+
+
+function gameController() {
+  state.word = wordArray[Math.floor(Math.random() * wordArray.length)];
+  let currentWord = document.querySelector("#current-word");
+  let gameBoard = document.querySelector("#game-board");
+  let message = document.querySelector("#message");
+  let letters = document.querySelector("#letters");
+  let alphabet = "abcdefghijklmnopqrstuvwxyzåäö".split("");
+  let word = state.word.split("");
+
+  renderCurrentWord(word, currentWord);
+  renderGameBoard();
+  renderLetters();
+  renderMessage(`Välj bokstav i fältet nedan. Du har ${6 - state.guesses} gissningar kvar.`);
+
+
 }
 
 window.onload = init;
--- js/views/game.js
+++ js/views/game.js
@@ -1,1 +1,10 @@
-export default ``;
+export default `
+<section id="current-word">
+  <p>Ord att gissa: </p>
+</section>
+
+<canvas id="game-board"></canvas>
+
+<section id="message"></section>
+
+<section id="letters"></section>`;
