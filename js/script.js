f1e3ddccd52111efdac7915ee26f844d2c578b0f
Stefan Åhlander <cs.ahlander@gmail.com>
2019-11-05 14:21:30 +0100

    Bugfix for not checking uppercase letters

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
-/** Import Array containing words. */
 import wordArray from "./models/word-array.js";
 
-/** Import views for the different stages of the game. */
 import initialView from "./views/initial.js";
 import gameView from "./views/game.js";
 import lossView from "./views/loss.js";
@@ -23,19 +21,17 @@ function renderView(view) {
 
 /** Render placeholders for the current word to be guessed. Loop over the word-Array
  *  and for each character create a span-element containing a "?". Append the elements
- *  to the section for the word in the game-view.
+ *  to the section for the word in the game-view. The true content of the letters
+ *  is irrelevant at this time.
  */
 function renderCurrentWord() {
-  state.wordArr.forEach((char, index) => {
+  state.wordArr.forEach(() => {
     let newChar = document.createElement("span");
     newChar.innerText = "?";
     state.currentWord.appendChild(newChar);
   });
 }
 
-/** Load the correct image depending on the number of spent guesses. Do this by updating
- *  the src property of the image-element allready stored in state.
- */
 function renderHangmanImage() {
   state.hangmanImage.src = `../images/h${state.guesses}.png`;
 }
@@ -52,27 +48,18 @@ function renderLetters() {
   })
 }
 
-/** Render a message for the player in teh message-section of the game-view. If no message
- *  is passed use a default message.
- */
 function renderMessage(message = `Du har ${6 - state.guesses} gissningar kvar.`) {
   state.messageBar.innerHTML = `<p>${message}</p>`;
 }
 
-/** Attach the event-handlers for in-game acions. For clicking letters and clicking the
- *  end-game button.
- */
 function attachGameControlls() {
   state.letters.addEventListener("click", letterClickHandler);
   state.endGame.addEventListener("click", init);
 }
 
-/** Eventhandler for clicking letters in the game-view. */
 function letterClickHandler(event) {
   /** Find out what letter/key was clicked. */
   let clickedChar = event.target;
-
-  /** Remove the letter/key that was clicked from the DOM. */
   clickedChar.parentElement.removeChild(clickedChar);
 
   /** Check if the clicked letter/key is present in the current word.
@@ -84,11 +71,8 @@ function letterClickHandler(event) {
    *  If not: Update guesses and render new message.
    */
   let char = clickedChar.dataset.char;
-
-  /** Check if the clicked letter is pressent in the word. */
   if (state.wordArr.includes(char)) {
-    /** Loop over all occurances of the char in the word-array. */
-    let index = state.wordArr.indexOf(char);
+    let index = state.wordArr.indexOf(char, 0);
     while (index != -1) {
       /** For each hit in the array change the "?" to the clicked letter/key. */
       state.currentWord.children[index + 1].innerText = char;
@@ -96,7 +80,6 @@ function letterClickHandler(event) {
        *   if the victory-condition is met.
        */
       state.wordArr[index] = true;
-      /** Increase index by one and do another index-search. */
       let continueFrom = ++index;
       index = state.wordArr.indexOf(char, continueFrom);
     }
@@ -109,49 +92,36 @@ function letterClickHandler(event) {
 
   /** Check for end or victory conditions. */
   if (state.guesses === state.maxguesses) {
-    /** Number of guesses equals max numer of guesses so the game is over. */
     renderView(lossView);
     lossController();
     return;
   }
 
   if (state.wordArr.every((item) => item === true)) {
-    /** The game has been won! */
     renderView(winView);
     winController();
   }
 }
 
 function init() {
-  /** Set or reset state to store game and DOM variables. */
   state = {
     guesses: 0,
     maxguesses: 6
   };
 
-  /** Render initial view and start. */
   renderView(initialView);
   initialController();
 }
 
-/** After the initial view is rendered wait for the player to press the
- *  start-button.
- */
 function initialController() {
-  /** Locate the start-button-element in the DOM. */
   let startBtn = document.querySelector("#startBtn");
 
-  /**
-   * Event handler for clicking the start button. After click remove the event-listner,
-   * render the game-view and go to the game-controller.
-   */
   function startGame() {
     startBtn.removeEventListener("click", startGame);
     renderView(gameView);
     gameController();
   };
 
-  /** Attach eventlistner to the start-button-element. */
   startBtn.addEventListener("click", startGame);
 }
 
@@ -164,9 +134,7 @@ function initialController() {
  *  - Calls the attachGameControlls-function to initialize the event handlers.   
  */
 function gameController() {
-  state.word = wordArray[Math.floor(Math.random() * wordArray.length)];
-  state.word = "hello"; // for testing
-  state.wordArr = state.word.split("");
+  state.wordArr = wordArray[Math.floor(Math.random() * wordArray.length)].toLowerCase().split("");
   state.currentWord = document.querySelector("#current-word");
   state.hangmanImage = document.querySelector("#hangman-image");
   state.messageBar = document.querySelector("#message-bar");
@@ -181,14 +149,9 @@ function gameController() {
   attachGameControlls();
 }
 
-/**
- * The game has been lost. The loss-view is allready rended so just attach an event handler
- * to the restart-button.
- */
 function lossController() {
   state.restart = document.querySelector("#restart");
 
-  /** When the restart-button is clicked restart the game by calling the init-function. */
   function restartGame() {
     state.restart.removeEventListener("click", restartGame);
     init();
@@ -205,6 +168,4 @@ function winController() {
   lossController();
 }
 
-/** When the document has finished loading fire the onload-event on window and run the 
-    init-function to start the game. */
 window.onload = init;
